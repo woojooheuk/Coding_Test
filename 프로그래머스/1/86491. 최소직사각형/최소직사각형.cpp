cb957c46c4def9d669ae3630ac840d8e28dec3bb
woojooheuk <55468500+woojooheuk@users.noreply.github.com>
2026-01-28 18:36:00 +0900

    Optimize size calculation in 최소직사각형.cpp
    Refactor to calculate Long and Short dimensions directly without using an additional vector.

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
@@ -12,21 +12,14 @@ int solution(vector<vector<int>> sizes) {
     int Size2 = 2;
     
     int Long = 0;
+    int Short = 0;
 
-    vector<int> Shorts;
-    
     for(int i = 0; i < Size1; ++i)
     {
         Long = max(Long,max(sizes[i][0],sizes[i][1]));
-        Shorts.push_back(min(sizes[i][0],sizes[i][1]));
+        Short = max(Short,min(sizes[i][0],sizes[i][1]));
     }
 
-    int Short = 0;
-    int Size3 = Shorts.size();
-    for(int i = 0; i < Size3; ++i)
-    {
-        Short = max(Short, Shorts[i]);
-    }
     answer = Long * Short;
     return answer;
 }
