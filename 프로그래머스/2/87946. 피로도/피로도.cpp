0cc5960521628f11a03b32ad5f6090d36320caa2
woojooheuk <55468500+woojooheuk@users.noreply.github.com>
2026-03-25 20:57:34 +0900

    [level 2] Title: 피로도, Time: 0.92 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -1,44 +1,38 @@
 #include <string>
 #include <vector>
-#include<algorithm>
-#include<iostream>
-#include<map>
 
 using namespace std;
 
-int DFS(vector<vector<int>>& Dungeons, vector<bool>& Visited, int Size, int k, int Num)
+int DFS(vector<vector<int>>& dungeons, vector<bool>& Visited, int Size, int k, int Num)
 {
-    //현재까지 회차에서 방문한 횟수
+    //현재까지 방문한 숫자 저장.
     int Count = Num;
     
-    //다음 방을 방문했을 때의 결과를 이번 방에서 미리 반영하지 마라.
     for(int i = 0; i < Size; ++i)
-    {       
-        //현재 가려는 곳이 이번 회차에서 갔던 곳인지 확인 + 피로도가 허용하는지. 
-        //안되면 다음으로 이동.
-        if(Visited[i] || k < Dungeons[i][0])
+    {
+        //스킵 조건 확인.
+        if(Visited[i] == true || k < dungeons[i][0])
             continue;
-            
-        //이번 회차에 여기 왔다 표시
-        Visited[i]=true;
-        //피로도 감소
         
-        //방문 횟수 증가
-        //다음 방문
-        int NewCount = DFS(Dungeons, Visited, Size, k-Dungeons[i][1], Num + 1);
-        Count = max(NewCount, Count);
-        //갔다가 나왔으니 해당 구역 방문 취소.
+        Visited[i] = true;
+        
+        //다음 방 탐색. 이번 방을 통과했다는 조건으로 피로도 감소, 방뭇 횟수 증가.
+        Count = max(DFS(dungeons, Visited, Size, k - dungeons[i][1], Num + 1),Count);
+        //이번 방을 들어갔을 때 할 수 있는 모든 경우의 수 탐색 완료. 방문 취소.
         Visited[i] = false;
     }
     return Count;
 }
 
 int solution(int k, vector<vector<int>> dungeons) {
-    int answer = 0;
+    int answer = -1;
+    //DFS. 자신이 방문했던 곳 표시.
+    //방문했거나, 최소 피로도보다 내 피로도가 작으면 스킵.
     
     int Size = dungeons.size();
     vector<bool> Visited;
     Visited.resize(Size, false);
+    
     answer = DFS(dungeons,Visited,Size,k,0);
     return answer;
 }
