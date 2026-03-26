c4af361da906556d0d7c5af78ff6401be8021c60
woojooheuk <55468500+woojooheuk@users.noreply.github.com>
2026-03-26 20:57:58 +0900

    [level 2] Title: 피로도, Time: 1.08 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.92 ms
+메모리: 4.13 MB, 시간: 1.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 25일 20:57:33
+2026년 03월 26일 20:57:59
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -1,38 +1,35 @@
 #include <string>
 #include <vector>
+#include<algorithm>
+#include<iostream>
 
 using namespace std;
 
-int DFS(vector<vector<int>>& dungeons, vector<bool>& Visited, int Size, int k, int Num)
-{
-    //현재까지 방문한 숫자 저장.
-    int Count = Num;
-    
-    for(int i = 0; i < Size; ++i)
-    {
-        //스킵 조건 확인.
-        if(Visited[i] == true || k < dungeons[i][0])
-            continue;
-        
-        Visited[i] = true;
-        
-        //다음 방 탐색. 이번 방을 통과했다는 조건으로 피로도 감소, 방뭇 횟수 증가.
-        Count = max(DFS(dungeons, Visited, Size, k - dungeons[i][1], Num + 1),Count);
-        //이번 방을 들어갔을 때 할 수 있는 모든 경우의 수 탐색 완료. 방문 취소.
-        Visited[i] = false;
-    }
-    return Count;
-}
-
 int solution(int k, vector<vector<int>> dungeons) {
     int answer = -1;
-    //DFS. 자신이 방문했던 곳 표시.
-    //방문했거나, 최소 피로도보다 내 피로도가 작으면 스킵.
+    //완전탐색. 가장 많이 방문한 던전 수.
+    //이미 방문한 곳은 방문하면 안됨.
+    for(auto& Vector : dungeons)
+        Vector.push_back(0);
     
-    int Size = dungeons.size();
-    vector<bool> Visited;
-    Visited.resize(Size, false);
+    //0은 탐사 안한 거, 1은 탐사한 거
+    int Num = 0;
+    function<void(int,int)> DFS;
+    DFS = [&](int Num, int k){
+        //현재 피로도와 최소 피로도 비교 및 Vector[2]가 0인지 확인.
+        answer = max(answer,Num);
+        for(auto& Vector : dungeons)
+        {
+            if(Vector[2] == 1 || k < Vector[0])
+                continue;
+            
+            Vector[2] = 1;
+            DFS(Num + 1, k - Vector[1]);
+            Vector[2] = 0;
+        }
+    };
     
-    answer = DFS(dungeons,Visited,Size,k,0);
+    DFS(0, k);
+    
     return answer;
 }
