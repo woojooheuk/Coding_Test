f09f21a18482a87a843ad7a0325fc906618b7962
woojooheuk <55468500+woojooheuk@users.noreply.github.com>
2026-05-15 20:22:18 +0900

    [level 2] Title: 올바른 괄호, Time: 0.32 ms, Memory: 3.91 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -1,31 +1,59 @@
 #include<string>
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
 bool solution(string s)
 {
     bool answer = false;
+    
+    //괄호 맞추기.
+    //string에 (면 더하고 )면 빼기?
+    string Temp = "";
+
     int Pair = 0;
     
-    int Size = s.size();
-    if(s[0] == ')' || s[Size] == '(')
-        return answer;
     for(char Word : s)
     {
-        switch(Word)
+        if(Word == '(')
+            Pair++;
+        else if(Word == ')')
         {
-            case '(':
-                Pair++;
-                break;
-            case ')':
-                Pair--;
+            Pair--;
+            if(Pair < 0)
                 break;
         }
-        if(Pair <0)
-            return answer;
     }
     if(Pair == 0)
-        answer = true;
+        answer = true; 
+    
+    
+    
+    
+    
+    
+    
+//     int Pair = 0;
+    
+//     int Size = s.size();
+//     if(s[0] == ')' || s[Size] == '(')
+//         return answer;
+//     for(char Word : s)
+//     {
+//         switch(Word)
+//         {
+//             case '(':
+//                 Pair++;
+//                 break;
+//             case ')':
+//                 Pair--;
+//                 break;
+//         }
+//         if(Pair <0)
+//             return answer;
+//     }
+//     if(Pair == 0)
+//         answer = true;
     return answer;
 }
