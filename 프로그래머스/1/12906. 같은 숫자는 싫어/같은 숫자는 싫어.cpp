37a444b5e90995825ea58513a8769efd10514ead
woojooheuk <55468500+woojooheuk@users.noreply.github.com>
2026-05-15 20:02:13 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 84.77 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -6,20 +6,37 @@ using namespace std;
 vector<int> solution(vector<int> arr) 
 {
     vector<int> answer;
+    answer.push_back(arr.front());
 
+    //배열에서 연속적으로 나타내는 숫자는 하나만 남기고 제거.
     int Size = arr.size();
-    
-    answer.push_back(arr[0]);
-    
-    for(int i = 0; i < Size; ++i)
+    for(int i = 1; i < Size; ++i)
     {
         if(answer.back() == arr[i])
+        {
             continue;
-        answer.push_back(arr[i]);
+        }
+        else
+        {
+            answer.push_back(arr[i]);
+        }
     }
     
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    cout << "Hello Cpp" << endl;
+    
+    
+//     int Size = arr.size();
+    
+//     answer.push_back(arr[0]);
+    
+//     for(int i = 0; i < Size; ++i)
+//     {
+//         if(answer.back() == arr[i])
+//             continue;
+//         answer.push_back(arr[i]);
+//     }
+    
+//     // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+//     cout << "Hello Cpp" << endl;
 
     return answer;
 }
